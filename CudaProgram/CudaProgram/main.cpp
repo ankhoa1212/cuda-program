d5d1e78de3c4782cf0e62e95837517b2d849e0f5
Austin <42307214+ankhoa1212@users.noreply.github.com>
2024-12-06 12:12:51 -0800

    Update main.cpp

--- CudaProgram/CudaProgram/main.cpp
+++ CudaProgram/CudaProgram/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include "main.h"
 
-// macro to replace <<<>>> to avoid visual studio error
+// macros to replace <<<>>> to avoid visual studio error
 #ifndef __INTELLISENSE__
 #define KERNEL_ARGS2(grid, block)                 <<< grid, block >>>
 #define KERNEL_ARGS3(grid, block, sh_mem)         <<< grid, block, sh_mem >>>
